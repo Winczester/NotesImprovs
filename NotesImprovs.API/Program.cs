a174b915560e74ef47d4d38365a06b7d7cbc119d
Dmytro Kurnitskiy <dmytro@straterix.com>
2024-11-26 15:24:35 +0200

    Added initial migration Fixed user login Added authorization to swagger
    (cherry picked from commit 85f240c140f77fb5807975cbc31724fe5feb5356)

--- NotesImprovs.API/Program.cs
+++ NotesImprovs.API/Program.cs
@@ -2,6 +2,7 @@ using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using NotesImprovs.BLL;
 using NotesImprovs.BLL.Interfaces;
 using NotesImprovs.BLL.Managers;
@@ -37,6 +38,7 @@ builder.Services.AddAuthentication(options =>
             IssuerSigningKey = new SymmetricSecurityKey(key)
         };
     });
+builder.Services.AddAuthorization();
 
 // Register services
 builder.Services.AddDatabaseServices(builder.Configuration);
@@ -46,7 +48,43 @@ builder.Services.AddBllServices();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc(
+        "v1",
+        new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "Notes API",
+            Description = "Notes API"
+        });
+    
+    var jwtSecurityScheme = new OpenApiSecurityScheme
+    {
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        Name = "JWT Authentication",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Description = "Put **_ONLY_** your JWT Bearer token on textbox below! (Example: eyJhbGciOiJIUz)",
+
+        Reference = new OpenApiReference
+        {
+            Id = JwtBearerDefaults.AuthenticationScheme,
+            Type = ReferenceType.SecurityScheme
+        }
+    };
+
+    // Adds the security definition to Swagger using the configured JWT security scheme
+    config.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
+
+    // Adds a security requirement to Swagger, specifying that the JWT security scheme must be used
+    config.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { jwtSecurityScheme, Array.Empty<string>() }
+    });
+});
+
 
 var app = builder.Build();
 
--- NotesImprovs.DAL/DependencyInjection.cs
+++ NotesImprovs.DAL/DependencyInjection.cs
@@ -1,6 +1,8 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using NotesImprovs.DAL.Models;
 
 namespace NotesImprovs.DAL;
@@ -10,16 +12,23 @@ public static class DependencyInjection
     public static IServiceCollection AddDatabaseServices(this IServiceCollection services, IConfiguration configuration)
     {
         // Retrieve the connection string from configuration
-        var connectionString = configuration.GetConnectionString("ConnectionString")
-                               ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        var connectionString = configuration.GetConnectionString("PostgreSQL")
+                               ?? throw new InvalidOperationException("Connection string 'PostgreSQL' not found.");
 
         // Configure Entity Framework to use SQL Server with the connection string
         services.AddDbContext<NotesImprovsDbContext>(options =>
             options.UseNpgsql(connectionString));
-        
-        services.AddIdentityCore<AppUser>()
-            .AddEntityFrameworkStores<NotesImprovsDbContext>();
 
+        services.AddIdentityCore<AppUser>(o =>
+            {
+                o.User.RequireUniqueEmail = true;
+                o.User.AllowedUserNameCharacters =
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890 \u0336-._";
+            })
+            .AddEntityFrameworkStores<NotesImprovsDbContext>()
+            .AddDefaultTokenProviders()
+            .AddSignInManager<SignInManager<AppUser>>();
+        
         return services;
     }
 }
